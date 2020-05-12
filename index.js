679fa79ed9cd55f4a5ea7141fdfb04b5687ae02c
Sena Heydari <sheydari@dosomething.org>
2020-05-12 02:30:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function johnLennonFacts (facts) {
   let excitedLennon = [];
   let i = 0;
   while (i < facts.length) {
-    excitedLennon.push(facts[0] + "!!!");
+    excitedLennon.push(facts[i] + "!!!");
   }
   return excitedLennon;
 }
