13c6b71d485cb1ffee7e315fd18889b10ed3e5ba
Sena Heydari <sheydari@dosomething.org>
2020-05-12 02:25:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 function theBeatlesPlay (array_musician, array_instrument) {
+  let theBand = {};
   // Set counter to start at 0, till max length of array.
-  for (let i = 0; i < )
+  for (let i = 0; i < array_musician.length; i++) {
+    theBand.push(array_musician[i] + " plays " + array_instrument[i]);
+  }
+  return theBand;
 }
